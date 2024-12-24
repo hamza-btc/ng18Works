54115be91cdefbf62099b0035fc02af896d6c84f
Hamza <h.rachid@hitech.fr>
2024-12-24 13:26:48 +0100

    integration toast et crud pour Todolist

--- src/app/pages/todos/todos.component.ts
+++ src/app/pages/todos/todos.component.ts
@@ -1,20 +1,39 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [FormsModule, NgClass],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css',
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit {
   title: string = 'Master you Day!';
   edit: boolean = false;
   defaultTodo: string = 'Write a todo...';
   shwoForm: boolean = false;
   todos: string[] = ['learn angular', 'React Js', 'Svelte', 'Spring'];
+  myTodo: string = '';
+  editable: boolean = false;
+  todoIndex: number | null = null;
+  notify: boolean = false;
+
+  ngOnInit() {}
 
   toggleForm() {
     this.shwoForm = !this.shwoForm;
+  }
+
+  triggerNotify(cusutmNotify: any) {
+    this.notification = {
+      ...cusutmNotify,
+    };
+    this.notify = true;
+    setTimeout(() => {
+      this.notify = false;
+    }, 2000);
   }
 
   showTitle() {
@@ -23,5 +42,91 @@ export class TodosComponent {
 
   changeToggleButton() {
     return this.shwoForm ? 'Hide' : 'Show';
+  }
+
+  addTodo() {
+    this.todos = [this.myTodo, ...this.todos];
+    this.initTodo();
+    this.triggerNotify({
+      message: 'Todo created Succefuly',
+      position: 'toast-top toast-end',
+      alertClass: 'alert-success',
+      duration: 90000,
+    });
+  }
+
+  editTodo(todo: string, index: number) {
+    this.myTodo = todo;
+    this.shwoForm = true;
+    this.editable = true;
+    this.todoIndex = index;
+    console.log(' this.todoIndex ', this.todoIndex);
+  }
+  updateTodo() {
+    if (this.todoIndex !== null) {
+      this.todos[this.todoIndex] = this.myTodo;
+      this.triggerNotify({
+        message: 'Todo updated Succefuly',
+        position: 'toast-up toast-end',
+        alertClass: 'alert-warning',
+        duration: 3000,
+      });
+    }
+    this.initTodo();
+  }
+
+  deleteTodo(index: number) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to delete Todo!",
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.triggerNotify({
+          message: 'Todo deleted Succefuly',
+          position: 'toast-middle toast-end',
+          alertClass: 'alert-error',
+          duration: 3000,
+        });
+        this.todos = this.todos.filter((item, i) => i !== index);
+        Swal.fire({
+          title: 'Todo is Deleted!',
+          text: 'Your todo  has been deleted.',
+          icon: 'success',
+          timer: 2000,
+          timerProgressBar: true,
+        });
+      }
+    });
+    // if (confirm(' Are you to sure de delete this task')) {
+    //   this.todos = this.todos.filter((item, i) => i !== index);
+    // }
+  }
+
+  initTodo() {
+    this.editable = false;
+    this.todoIndex = null;
+    this.shwoForm = false;
+  }
+
+  notification = {
+    message: '',
+    position: '',
+    alertClass: '',
+    duration: 3000,
+  };
+
+  cancel() {
+    this.initTodo();
+    this.triggerNotify({
+      message: 'Updated canceld',
+      position: 'toast-end',
+      alertClass: 'alert-error',
+      duration: 2000,
+    });
   }
 }
